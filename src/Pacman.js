24ee40311766ce83a1484191d84f2371e95fdf0a
nhhongan <nhhongan@gmail.com>
2024-05-11 22:21:15 +0700

    add moving direction

--- src/Pacman.js
+++ src/Pacman.js
@@ -1,13 +1,51 @@
 export default class Pacman {
-    constructor(x, y, tileSize, velocity, tileMap) {
+    constructor(x, y, tileSize, speed, tileMap) {
         this.x = x;
         this.y = y;
         this.tileSize = tileSize;
-        this.velocity = velocity;
+        this.speed = speed;
         this.tileMap = tileMap;
+        this.#loadPacmanImages();
     }
 
     draw(ctx){
-        
+
+    }
+
+    moveProcess() {
+        this.changeDirectionPossible();
+        this.moveForwards();
+        if (this.checkCollision()){
+            this.checkCollision();
+        }
+    }
+
+    eat() {
+
+    }
+
+    // moveBackwards() {
+
+    // }
+
+    // moveForwards() {
+
+    // }
+
+    checkCollision() {
+
+    }
+
+    checkGhostCollision() {
+
+    }
+
+    changeDirectionPossible(){}
+
+    changeAnimation(){}
+
+    #loadPacmanImages(){
+        this.pacmanImages = new Image();
+        this.pacmanImages.src = "../assets/animations.gif";
     }
 }

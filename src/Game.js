750a08c1dd043b7babb13a8d311d4bb5f70cdcf4
nhhongan <nhhongan@gmail.com>
2024-05-19 19:30:21 +0700

    add end screen

--- src/Game.js
+++ src/Game.js
@@ -19,6 +19,7 @@ const gameWinSound = new Audio('../assets/sounds/gameWin.wav');
 
 function gameLoop() {
     tileMap.draw(canvas, ctx);
+    drawGameEnd();
     pacman.draw(ctx, pause(), enemies);
     enemies.forEach(enemy => enemy.draw(ctx, pause(), pacman));
     checkGameOver();
@@ -50,6 +51,24 @@ function isGameOver() {
 
 function pause() {
     return !pacman.madeFirstMove || gameOver || gameWin;
+}
+
+function drawGameEnd() {
+    if (gameOver || gameWin) {
+        let text = "You Win!!!";
+        if (gameOver) {
+            text = "Game Over";
+        }
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, canvas.height/2.5, canvas.width, 150);
+        ctx.font = '80px comic sans';
+        const gradient = ctx.createLinearGradient(0,0,canvas.width, canvas.height, 0);
+        gradient.addColorStop('0','magenta');
+        gradient.addColorStop('0.5','blue');
+        gradient.addColorStop('1.0','red');
+        ctx.fillStyle = gradient;
+        ctx.fillText(text, 150, canvas.height/1.9);
+    }
 }
 
 
